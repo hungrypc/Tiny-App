d0f5448a090f99ca02cfa045d174ef69b24ffe9e
Phil <philipchanhk626@gmail.com>
2019-06-04 19:41:38 +0000

    added url routes

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,14 +13,24 @@ app.get("/", (request, response) => {
   response.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
-});
+// app.get("/urls.json", (request, response) => {
+//   response.json(urlDatabase);
+// });
 
 app.get("/hello", (request, response) => {
   response.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render('urls_index', templateVars);
+});
+
+app.get("/urls/:shortURL", (request, response) => {
+  let templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase};
+  response.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
